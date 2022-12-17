b9b4466183f1b39c3b26d1330b02755c4342dcaf
unknown <runju.raton@gmail.com>
2022-12-17 16:17:32 +0600

    Image Detectio

--- app/Modules/Users/Views/admin/create.blade.php
+++ app/Modules/Users/Views/admin/create.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.admin')
 @section('styles')
     @include('partials.steps-form')
+    <link href="{{ asset('assets/common/plugins/croppie-2.6.5/croppie.css') }}" rel="stylesheet">
+    <script src="{{ asset('assets/common/plugins/croppie-2.6.5/croppie.js') }}"></script>
+    <script src="{{ asset('assets/common/plugins/jquery-facedetection/jquery.facedetection.min.js') }}"></script>
 @endsection
 @section('breadcrumb')
     <nav aria-label="breadcrumb">
@@ -114,7 +117,8 @@
                             <div class="input-group-prepend">
                                 <label class="input-group-text"><strong>Date Of Birth</strong></label>
                             </div>
-                            <input type="text" name="date_of_birth" id="date_of_birth" class="form-control dob-datepicker"
+                            <input type="text" name="date_of_birth" id="date_of_birth"
+                                   class="form-control dob-datepicker"
                                    placeholder=""/>
                             @if ($errors->has('date_of_birth'))
                                 <span class="text-danger fs-6">
@@ -196,7 +200,8 @@
                         <div class="form-group">
                             <label class="form-label">Profile Picture</label>
                             <input class="form-control" type="file" name="picture"
-                                   onchange="jsPreviewUploadedImage(this,'preview_picture')"/>
+                                   onchange="imageUploadWithCroppingAndDetect(this, 'preview_picture', 'picture')"
+                            />
                             @if ($errors->has('picture'))
                                 <span class="text-danger fs-6">
                                                 <strong>{{ $errors->first('picture') }}</strong>
@@ -228,12 +233,43 @@
         </div><!-- #/wizard -->
         <form/>
 
-
+        <!-- Start -:- Image Upload Modal -->
+        <div class="modal fade" id="image_upload_modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+             aria-labelledby="image_upload_modal_label" aria-hidden="true">
+            <div class="modal-dialog modal-md">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="image_upload_modal_label">Image Uplaod Modal</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div id="image_upload_modal_crop_area" class="center-block"></div>
+                    </div><!-- ./modal-body -->
+                    <div class="modal-footer">
+                        <div class="alert alert-success" role="alert">
+                            <i class="fa fa-spinner fa-pulse"></i> Please wait, Face detecting
+                        </div>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Understood</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End -:- Image Upload Modal -->
         @endsection
         @section('scripts')
             <script type="text/javascript">
-                $(document).ready(function () {
+                var image_upload_modal_id = 'image_upload_modal';
+                var image_upload_modal_crop_area_id = 'image_upload_modal_crop_area';
+                var image_upload_modal_img_id = 'image_upload_modal_img';
+                var image_upload_modal_viewport_width = 300;
+                var image_upload_modal_viewport_height = 300;
+                var image_upload_modal_is_required = false;
+                var image_upload_modal_face_detect = false;
+                var image_upload_modal_img, image_upload_modal_raw_img, image_upload_modal_preview_img_id,
+                    image_upload_modal_base64_value_target,image_upload_modal_input_field;
 
+                $(document).ready(function () {
                     $("#wizard").steps({
                         headerTag: "h2",
                         bodyTag: "fieldset",
@@ -274,5 +310,132 @@
                         reader.readAsDataURL(file);
                     }
                 }// end -:- jsPreviewUploadedImage()
+
+                function imageUploadWithCroppingAndDetect(input, img_preview_id, base64_value_target) {
+                    if (input.files && input.files[0]) {
+                        var mime_type = input.files[0].type;
+                        if (!(mime_type == 'image/jpeg' || mime_type == 'image/jpg' || mime_type == 'image/png')) {
+                            alert('Image format is not valid. Only PNG or JPEG or JPG type images are allowed !');
+                            return false;
+                        }
+                        // Configure dynamic data
+                        image_upload_modal_input_field = input;
+                        image_upload_modal_preview_img_id = img_preview_id;
+                        image_upload_modal_base64_value_target = base64_value_target;
+                        // Configure viewport height, width from input
+                        if (input.getAttribute('size')) {
+                            var viewport_size = input.getAttribute('size').split('x');
+                            image_upload_modal_viewport_width = parseInt(viewport_size[0]);
+                            image_upload_modal_viewport_height = parseInt(viewport_size[1]);
+                        }
+                        // enable face detect
+                        image_upload_modal_face_detect = true;
+                        var reader = new FileReader();
+                        reader.onload = function (e) {
+                            // Show modal
+                            $('#' + image_upload_modal_id).modal('show');
+                            // Create image element to face detect
+                            document.getElementById(image_upload_modal_crop_area_id).innerHTML = "";
+                            var img_elem = document.createElement("img");
+                            img_elem.id = image_upload_modal_img_id;
+                            img_elem.style.height = '100%';
+                            img_elem.style.width = '100%';
+                            img_elem.src = e.target.result;
+                            document.getElementById(image_upload_modal_crop_area_id).appendChild(img_elem);
+                            // End Create image element to face detect
+
+                            // re-set upload cropping area
+                            image_upload_modal_img = $('#' + image_upload_modal_img_id);
+                            //console.log(image_upload_modal_img);
+                            // Set raw image to use later
+                            image_upload_modal_raw_img = e.target.result;
+                        };
+                        reader.readAsDataURL(input.files[0]);
+                    } else {
+                        alert('Uploaded input file not correct, please upload again !');
+                    }
+                }// end -:- imageUploadWithCroppingAndDetect()
+                $('#' + image_upload_modal_id).on('shown.bs.modal', function (e) {
+                    // If already croppie initiated, then destroy it
+                    if (image_upload_modal_img.data('croppie')) {
+                        image_upload_modal_img.croppie('destroy');
+                    }
+                    // Set viewport height, width for cropping
+                    document.getElementById(image_upload_modal_crop_area_id).style.width = (image_upload_modal_viewport_width + 50) + "px";
+                    document.getElementById(image_upload_modal_crop_area_id).style.height = (image_upload_modal_viewport_height + 50) + "px";
+                    if (image_upload_modal_face_detect) {
+                        setTimeout(function () {
+                            image_upload_modal_img.faceDetection({
+                                complete: function (faces) {
+                                    if (faces.length > 0) {
+                                        //console.log(image_upload_modal_img);
+                                        image_upload_modal_img.croppie({
+                                            viewport: {
+                                                width: image_upload_modal_viewport_width,
+                                                height: image_upload_modal_viewport_height,
+                                                type: 'square'
+                                            },
+                                            enforceBoundary: true, // Restricts zoom so image cannot be smaller than viewport
+                                            enableResize: false, // Enable or disable support for resizing the viewport area
+                                        });
+                                        console.log('faceDetection.complete');
+                                    } else {
+                                        alert('Given image is not valid , (Can\'t recognize any face) !');
+                                        document.getElementById(image_upload_modal_img_id).remove();
+                                        $('#' + image_upload_modal_id).modal('hide');
+                                    }
+                                }
+                            });
+                        }, 3000);
+                    } else {
+                        image_upload_modal_img.croppie({
+                            viewport: {
+                                width: image_upload_modal_viewport_height,
+                                height: image_upload_modal_viewport_height,
+                                type: 'square'
+                            },
+                            enforceBoundary: true, // Restricts zoom so image cannot be smaller than viewport
+                            enableResize: false, // Enable or disable support for resizing the viewport area
+                        });
+                        // Bind raw image to croppie
+                        image_upload_modal_img.croppie('bind', {
+                            url: image_upload_modal_raw_img
+                        }).then(function () {
+                            console.log('jQuery bind complete');
+                        });
+                    }
+                });// end -:- Image Modal Show Event
+                $('#imageUplaodModal-cropImageBtn').on('click', function (ev) {
+                    upload_crop_area.croppie('result', {
+                        type: 'base64',
+                        format: 'jpeg',
+                        // size: 'original',
+                        size: {width: viewport_width, height: viewport_height}
+                    }).then(function (resp) {
+                        document.getElementById(img_preview_div_id).setAttribute('src', resp);
+                        document.getElementById(base64_value_field_id).value = resp;
+
+                        document.getElementById(input_field.id).setAttribute('disabled', true);
+                        $('#correspondent_signature').hide();
+
+                        // Create reset button
+                        var preview_parent_div = document.getElementById(img_preview_div_id).parentNode.parentNode;
+                        var btn_elem = document.createElement("button");
+                        btn_elem.type = 'button';
+                        btn_elem.id = 'reset_image_'.concat(input_field.id);
+                        btn_elem.innerHTML = 'Reset image';
+                        btn_elem.className = 'btn btn-warning btn-xs';
+                        btn_elem.value = [input_field.id, base64_value_field_id, img_preview_div_id];
+                        btn_elem.onclick = function () {
+                            // resetImage(input_field.id, base64_value_field_id, img_preview_div_id, this);
+                            resetImage(btn_elem.value, this);
+                        };
+                        preview_parent_div.parentNode.insertBefore(btn_elem, preview_parent_div.nextSibling);
+                        // End Create reset button
+
+                        $('#' + Image_upload_modal_id).modal('hide');
+                    });
+                    upload_crop_area.croppie('destroy');
+                });
             </script>
 @endsection
